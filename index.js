f77f08e53986cb6a6920dba3788ceed4b759fb4f
MickeyWeiss <167358315+MickeyWeiss@users.noreply.github.com>
2024-08-07 18:52:35 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -7,19 +7,63 @@ app.use(express.json());
 app.use(require('morgan')('dev'));
 
 app.post('/api/flavors', async (req, res, next) => {
-
+    try {
+        const SQL = `
+            INSERT INTO flavors(flavor);
+            VALUES($1);
+            RETURNING *;`
+        const response = await client.query(SQL, [req.body.flavor])
+        res.send(response.rows[0])
+    }catch (express) {
+        next(express)
+    }
 })
 app.get('/api/flavors', async (req, res, next) => {
-
+    try {
+        const SQL = `
+            SELECT * from flavors;`
+        const response = await client.query(SQL);
+        res.send(response.rows);
+    }catch(express){
+        next(express)
+    }
 })
 app.get('/api/flavors/:id', async (req, res, next) => {
-    
+        try {
+            const SQL = `
+                WHERE id = $1;
+                RETURNING *;`
+
+            const response = await client.query(SQL, [req.body.flavor])
+            res.send(respponse.rows[0])
+        } catch(express) {
+            next(express)
+        }
 })
 app.put('/api/flavors/:id', async (req, res, next) => {
+    try{
+        const SQL = `
+            UPDATE flavors;
+            SET flavor = $1, is_favorite = $2, updated_at = now();
+            WHERE id = $3;
+            RETURNING *`
 
+        const response = await client.query(SQL, [req.body.flavor, req.body.is_favorite, req.params.id]);
+        res.send(response.rows[0])
+    }catch (express) {
+        next(express)
+    }
 })
 app.delete('/api/flavors/:id', async (req, res, next) => {
-
+    try {
+        const SQL = `
+            DELETE from flavors;
+            WHERE id = $1;`
+        const response = await client.query(SQL, [req.params.id])
+        response.sendStatus
+    }catch (express) {
+        next(express)
+    }
 })
 
 const init = async () => {
@@ -36,6 +80,9 @@ const init = async () => {
     INSERT INTO flavors (flavor, is_favorite) VALUES('strawberry', false);`;
     await client.query(SQL);
     console.log('data seeded')
+
+    const port = process.env.PORT || 3000;
+    app.listen(port, () => console.log(`listening on port ${port}`))
 }
 
 init();
